39ad6210c6e84ae5b76c6d86d43b8fe87a4223a0
Iryna-Slynko <53280378+Iryna-Slynko@users.noreply.github.com>
2020-09-18 13:50:00 +0000

    comments

--- Advance_sort_dictionary.py
+++ Advance_sort_dictionary.py
@@ -1,3 +1,6 @@
+'''Given the dictionary, medals, now sort by the medal count. 
+Save the three countries with the highest medal count to the list, top_three.'''
+
 medals = {'Japan':41, 'Russia':56, 'South Korea':21, 'United States':121, 'Germany':42, 'China':70}
 top_three=sorted(medals, key=lambda c: medals[c], reverse=True)[:3]
 print(top_three)
